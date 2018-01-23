3c5b6e8f580cda9a630f7fb079a707c82ad2c2ba
t0n9t0n9 <1776452157@qq.com>
2018-01-23 12:16:05 +0800

    update

--- soft_download.sh
+++ soft_download.sh
@@ -10,3 +10,5 @@ apt-get install goldendict
 apt-get install libreoffice
 #安装chrome
  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#自动解决依赖问题
+apt-get install gdebi
